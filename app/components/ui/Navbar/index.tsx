564cfa7aeceaf3ab9eb410a4edbd7aa27b5ae043
ali-khosravani <ali.p3soft@gmail.com>
2024-01-26 20:32:34 +0330

    Fail navbar

--- app/components/ui/Navbar/index.tsx
+++ app/components/ui/Navbar/index.tsx
@@ -2,26 +2,26 @@
 export const NavbarHeader = () => {
   return (
     <>
-      <nav className="navbar navbar-dark navbar-expand-md fixed-top w-100 bg-dark d-flex justify-content-between">
+      <nav className="navbar navbar-expand-md fixed-top w-100 d-flex justify-content-between">
         <div className="container-fluid">
-          <div className="text-white navbar-brand">
-            <a href="/">شکوفالند</a>
+          <div className="navbar-brand">
+            <a href="/" className="text-black">شکوفالند</a>
           </div>
 
-          <button className="navbar-toggler text-white" type="button" data-toggle="collapse"
-            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarcollapse" aria-controls="navbarcollapse"
             aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="navbar-collapse collapse justify-content-center" id="navbarSupportedContent">
+          <div className="navbar-collapse collapse justify-content-center" id="navbarcollapse">
             <ul className="navbar-nav">
-              <li className="nav-item p-2"><a href="/" className="text-white"><i className="bi bi-house text-white"></i></a></li>
-              <li className="nav-item p-2"><a href="/attendance" className="text-white">کنترل تردد</a></li>
-              <li className="nav-item p-2"><a href="/hardware" className="text-white">سخت افزار</a></li>
-              <li className="nav-item p-2"><a href="/software" className="text-white">نرم افزار</a></li>
-              <li className="nav-item p-2"><a href="/blog" className="text-white">بلاگ</a></li>
-              <li className="nav-item p-2"><a href="/Contact" className="text-white">ارتباط با ما</a></li>
-              <li className="nav-item p-2"><a href="/About" className="text-white">درباره ما</a></li>
+              <li className="nav-item p-2"><a href="/"><i className="bi bi-house"></i></a></li>
+              <li className="nav-item p-2"><a href="/attendance">کنترل تردد</a></li>
+              <li className="nav-item p-2"><a href="/hardware">سخت افزار</a></li>
+              <li className="nav-item p-2"><a href="/software">نرم افزار</a></li>
+              <li className="nav-item p-2"><a href="/blog">بلاگ</a></li>
+              <li className="nav-item p-2"><a href="/Contact">ارتباط با ما</a></li>
+              <li className="nav-item p-2"><a href="/About">درباره ما</a></li>
             </ul>
           </div>
         </div>
