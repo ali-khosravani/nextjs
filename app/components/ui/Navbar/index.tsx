77ed88f12741f1717ac5245f1cd8cfdf7fec48b9
ali-khosravani <ali.p3soft@gmail.com>
2024-01-26 15:27:08 +0330

    add menu

--- app/components/ui/Navbar/index.tsx
+++ app/components/ui/Navbar/index.tsx
@@ -1,14 +1,33 @@
-import Link from "next/link"
 
 export const NavbarHeader = () => {
   return (
     <>
-    <nav>
-    <Link href={"/"}>Home</Link>
-    <Link href={"/Contact"}>Contact</Link>
-    <Link href={"/About"}>About</Link>
-    </nav>
-      
+      <nav className="navbar navbar-dark navbar-expand-md fixed-top w-100 bg-dark d-flex justify-content-between">
+        <div className="container-fluid">
+          <div className="text-white navbar-brand">
+            <a href="/">شکوفالند</a>
+          </div>
+
+          <button className="navbar-toggler text-white" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+
+          <div className="navbar-collapse collapse justify-content-center" id="navbarSupportedContent">
+            <ul className="navbar-nav">
+              <li className="nav-item p-2"><a href="/" className="text-white"><i className="bi bi-house text-white"></i></a></li>
+              <li className="nav-item p-2"><a href="/attendance" className="text-white">کنترل تردد</a></li>
+              <li className="nav-item p-2"><a href="/hardware" className="text-white">سخت افزار</a></li>
+              <li className="nav-item p-2"><a href="/software" className="text-white">نرم افزار</a></li>
+              <li className="nav-item p-2"><a href="/blog" className="text-white">بلاگ</a></li>
+              <li className="nav-item p-2"><a href="/Contact" className="text-white">ارتباط با ما</a></li>
+              <li className="nav-item p-2"><a href="/About" className="text-white">درباره ما</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "bootstrap/dist/css/bootstrap.rtl.css"
+import "bootstrap-icons/font/bootstrap-icons.css"
 import "./globals.css";
 
 export const metadata: Metadata = {
