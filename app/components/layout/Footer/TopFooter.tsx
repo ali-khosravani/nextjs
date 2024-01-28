87899456b2e9a977989a00cae3abac8a5f2a5337
ali-khosravani <ali.p3soft@gmail.com>
2024-01-28 23:54:48 +0330

    Add topFooter

---
+++ app/components/layout/Footer/TopFooter.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const TopFooter = () => {
+  return (
+    <>
+      <footer className='bg-light h-40'>
+
+        <div className='container-fluid'>
+          <div className='w-100 d-flex justify-content-between border border-3'>
+            <div className='col-3 p-3 m-2'>
+              <div>
+                <h3>About</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae illo obcaecati sapiente voluptatibus qui velit repellat? Nisi id eaque omnis aliquam molestias dignissimos cupiditate dolorum? Distinctio architecto sapiente odio corporis!
+
+                </p>
+              </div>
+
+            </div>
+            <div className='col-3 p-3 m-2'>
+              <div> <h3>Contact</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae illo obcaecati sapiente voluptatibus qui velit repellat? Nisi id eaque omnis aliquam molestias dignissimos cupiditate dolorum? Distinctio architecto sapiente odio corporis!
+
+                </p></div>
+            </div>
+            <div className='col-3 p-3 m-2'>
+             <div>
+             <h3>Blog</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae illo obcaecati sapiente voluptatibus qui velit repellat? Nisi id eaque omnis aliquam molestias dignissimos cupiditate dolorum? Distinctio architecto sapiente odio corporis!
+
+              </p>
+             </div>
+
+            </div>
+            <div className='col-3 p-3 m-2'>
+
+              <div><h3>web</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae illo obcaecati sapiente voluptatibus qui velit repellat? Nisi id eaque omnis aliquam molestias dignissimos cupiditate dolorum? Distinctio architecto sapiente odio corporis!
+
+                </p></div>
+            </div>
+          </div>
+        </div>
+
+      </footer>
+    </>
+  )
+}
+
+export default TopFooter
--- app/components/ui/Navbar/index.tsx
+++ app/components/ui/Navbar/index.tsx
@@ -1,4 +1,3 @@
-import './Styled.css'
 
 export const NavbarHeader = () => {
   return (
