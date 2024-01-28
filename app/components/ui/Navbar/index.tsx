27e79fb83a22982607cc618060ac0322c7d4d83a
ali-khosravani <ali.p3soft@gmail.com>
2024-01-28 22:41:58 +0330

    Update UI navbar and add font

--- app/components/ui/Navbar/index.tsx
+++ app/components/ui/Navbar/index.tsx
@@ -1,32 +1,35 @@
 
 export const NavbarHeader = () => {
   return (
-    
-      <nav className="navbar navbar-expand-md fixed-top w-100 d-flex justify-content-between">
-        <div className="container-fluid">
-          <div className="navbar-brand">
-            <a href="/" className="text-black">شکوفالند</a>
-          </div>
 
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarcollapse" aria-controls="navbarcollapse"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="navbar-collapse collapse justify-content-center" id="navbarcollapse">
-            <ul className="navbar-nav">
-              <li className="nav-item p-2"><a href="/"><i className="bi bi-house"></i></a></li>
-              <li className="nav-item p-2"><a href="/attendance">کنترل تردد</a></li>
-              <li className="nav-item p-2"><a href="/hardware">سخت افزار</a></li>
-              <li className="nav-item p-2"><a href="/software">نرم افزار</a></li>
-              <li className="nav-item p-2"><a href="/blog">بلاگ</a></li>
-              <li className="nav-item p-2"><a href="/Contact">ارتباط با ما</a></li>
-              <li className="nav-item p-2"><a href="/About">درباره ما</a></li>
-            </ul>
-          </div>
+    <nav className="navbar navbar-expand-md fixed-top w-100 d-flex justify-content-between border-bottom border-3">
+      <div className="container-fluid">
+        <div className="navbar-brand">
+          <a href="/" className="text-black">شکوفالند</a>
         </div>
-      </nav>
-  
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+          data-bs-target="#navbarcollapse" aria-controls="navbarcollapse"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navbar-collapse collapse justify-content-center" id="navbarcollapse">
+          <ul className="navbar-nav">
+            <li className="nav-item p-3"><a href="/"><i className="bi bi-house"></i></a></li>
+            <li className="nav-item p-3"><a href="/attendance">کنترل تردد</a></li>
+            <li className="nav-item p-3"><a href="/hardware">سخت افزار</a></li>
+            <li className="nav-item p-3"><a href="/software">نرم افزار</a></li>
+            <li className="nav-item p-3"><a href="/blog">بلاگ</a></li>
+            <li className="nav-item p-3"><a href="/Contact">ارتباط با ما</a></li>
+            <li className="nav-item p-3"><a href="/About">درباره ما</a></li>
+          </ul>
+        </div>
+        <form action="">
+          <button className="btn btn-primary p-2 m-2">ثبت نام</button>
+          <button className="btn btn-primary p-2 m-2">ورود</button>
+        </form>
+      </div>
+    </nav>
   )
 }
 
