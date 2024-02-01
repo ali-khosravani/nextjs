fac705f36fe31a0dbdf00036742ae4f4825ab18e
ali-khosravani <ali.p3soft@gmail.com>
2024-02-02 00:54:54 +0330

    top header responsive finished

--- app/components/layout/Header/TopHeader.tsx
+++ app/components/layout/Header/TopHeader.tsx
@@ -9,23 +9,23 @@ import envelope from '@/public/icons/public-icon/envelope-solid.svg'
 const TopHeader = () => {
   return (
     <div className="container-fluid">
-      <div className="row justify-content-center justify-content-md-around">
-        <div className="col-auto">
+      <div className="row justify-content-center justify-content-lg-around">
+        <div className="col-lg-6 col-md-8">
           <ul className="list-inline mb-0 contact-box">
             <li className="list-inline-item">
-              <p className='px-1 mb-0 d-inline-block'>0912-7622665</p>
+              <p className='mb-0 d-inline-block'>0912-7622665</p>
               <Image src={telephone} alt='Fullow us' height={20} width={20} />
             </li>
             <li className="list-inline-item">
-              <p className='px-1 mb-0 d-inline-block'>ali.khosravani.f@gmail.com</p>
+              <p className='mb-0 d-inline-block'>ali.khosravani.f@gmail.com</p>
               <Image src={envelope} alt='Fullow us' height={20} width={20} />
             </li>
           </ul>
         </div>
-        <div className="col-auto mt-3 mt-md-0">
+        <div className="col-lg-4 col-md-4 mt-3 mt-md-0">
           <SocialMedia />
         </div>
-        <div className="col-auto">
+        <div className="col-lg-2 d-none d-lg-block">
           <button className='btn btn-link btn-sm'>ورود</button>
           <button className='btn btn-link btn-sm'>ثبت نام</button>
         </div>
