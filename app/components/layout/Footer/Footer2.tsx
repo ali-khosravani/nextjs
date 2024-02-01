ace0ce12c7476729c64c0f09f938f0ef40b3131f
ali-khosravani <ali.p3soft@gmail.com>
2024-02-01 09:41:51 +0330

    Update UI

--- app/components/layout/Footer/Footer2.tsx
+++ app/components/layout/Footer/Footer2.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import './Styled.css';
-
+import '../../../../public/fontawesome-free-6.5.1-web/css/all.css'
 
 const Footer2 = () => {
     return (
         <>
-            <div className="container footer2Class">
-                <div className="row h-100">
-                    <div className='col'>
-                        <h5 className='text-center'>ما را در شبکه های اجتماعی دنبال کنید</h5>
-                    </div>
-                    <div className='col'>
-                        <div className='d-flex justify-content-center'>
-                            <h3><a href="/"><i className="bi bi-instagram p-2"></i></a></h3>
-                            <h3><a href="/"><i className="bi bi-telegram p-2"></i></a></h3>
-                            <h3><a href="/"><i className="bi bi-github p-2"></i></a></h3>
-                            <h3><a href="/"><i className="bi bi-whatsapp p-2"></i></a></h3>
+            <div className="container-fluid my-3">
+                <div className="container py-3 social-box rounded-5">
+                    <div className="row justify-content-between">
+                        <div className="col-auto">
+                            <h4 className='mb-0'>ارائه جدیدترین دستگاه های فلزیاب و X-Ray</h4>
+                        </div>
+                        <div className="col-auto">
+                            <div className="social-box-ul">
+                                <span className='d-inline-block mb-0'>مارا در شبکه های اجتماعی دنبال کنید</span>
+                                <ul className="list-inline d-inline-block mb-0 px-2">
+                                    <li className="list-inline-item"><a href="/"><i className="fa-brands fa-instagram"></i></a></li>
+                                    <li className="list-inline-item"><a href="/"><i className="fa-brands fa-whatsapp"></i></a></li>
+                                    <li className="list-inline-item"><a href="/"><i className="fa-brands fa-telegram"></i></a></li>
+                                    <li className="list-inline-item"><a href="/"><i className="fa-brands fa-github"></i></a></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </>
     )
 }
--- app/components/layout/Footer/TopFooter.tsx
+++ app/components/layout/Footer/TopFooter.tsx
@@ -3,69 +3,64 @@ import React from 'react'
 const TopFooter = () => {
   return (
     <div className='container-fluid'>
-      <div className="row">        
-          <div className='col-lg-3 col-md-6 col-sm-12 col-12'>
-            <div className='m-1 p-1'>
-              <h4>خدمات مشتریان</h4>
-              <ul>
-                <li className="pt-2"><a href="/">کنترل تردد</a></li>
-                <li className="pt-2"><a href="/">سخت افزار</a></li>
-                <li className="pt-2"><a href="/">نرم افزار</a></li>
-                <li className="pt-2"><a href="/">بازرسی</a></li>
-                <li className="pt-2"><a href="/">برد کنترل</a></li>
-              </ul>
-            </div>
+      <div className="row">
+        <div className='col-lg-3 col-md-6 col-sm-12 col-12'>
+          <div className='m-1 p-1'>
+            <h4>خدمات مشتریان</h4>
+            <ul>
+              <li className="pt-2"><a href="/">کنترل تردد</a></li>
+              <li className="pt-2"><a href="/">سخت افزار</a></li>
+              <li className="pt-2"><a href="/">نرم افزار</a></li>
+              <li className="pt-2"><a href="/">بازرسی</a></li>
+              <li className="pt-2"><a href="/">برد کنترل</a></li>
+            </ul>
+          </div>
 
+        </div>
+        <div className='col-lg-3 col-md-6 col-sm-12 col-12'>
+          <div className='m-1 p-1'>
+            <h4>راهنمای استفاده</h4>
+            <ul>
+              <li className="pt-2"><a href="/">خدمات پشتیبانی</a></li>
+              <li className="pt-2"><a href="/">آموزش</a></li>
+              <li className="pt-2"><a href="/">انتقادات و پیشنهادات</a></li>
+              <li className="pt-2"><a href="/">خدمات ضمانت کالا</a></li>
+              <li className="pt-2"><a href="/"> </a></li>
+            </ul>
           </div>
-          <div className='col-lg-3 col-md-6 col-sm-12 col-12'>
-            <div className='m-1 p-1'>
-              <h4>راهنمای استفاده</h4>
-              <ul>
-                <li className="pt-2"><a href="/">خدمات پشتیبانی</a></li>
-                <li className="pt-2"><a href="/">آموزش</a></li>
-                <li className="pt-2"><a href="/">انتقادات و پیشنهادات</a></li>
-                <li className="pt-2"><a href="/">خدمات ضمانت کالا</a></li>
-                <li className="pt-2"><a href="/"> </a></li>
-              </ul>
-            </div>
+        </div>
+        <div className='col-lg-3 col-md-6 col-sm-12 col-12'>
+          <div className='m-1 p-1'>
+            <h4>دسترسی سریع</h4>
+            <ul>
+              <li className="pt-2"><a href="/Rule">قوانین</a></li>
+              <li className="pt-2"><a href="/About">درباره ما</a></li>
+              <li className="pt-2"><a href="/Contact">ارتباط با ما</a></li>
+              <li className="pt-2"><a href="/">همکاری با ما</a></li>
+              <li className="pt-2"><a href="/Privacy">حریم خصوصی</a></li>
+              <li className="pt-2"><a href="/Faq">سوالات متداول</a></li>
+            </ul>
           </div>
-          <div className='col-lg-3 col-md-6 col-sm-12 col-12'>
-            <div className='m-1 p-1'>
-              <h4>دسترسی سریع</h4>
-              <ul>
-                <li className="pt-2"><a href="/Rule">قوانین</a></li>
-                <li className="pt-2"><a href="/About">درباره ما</a></li>
-                <li className="pt-2"><a href="/Contact">ارتباط با ما</a></li>
-                <li className="pt-2"><a href="/">همکاری با ما</a></li>
-                <li className="pt-2"><a href="/Privacy">حریم خصوصی</a></li>
-                <li className="pt-2"><a href="/Faq">سوالات متداول</a></li>
-              </ul>
-            </div>
 
+        </div>
+        <div className='col-lg-3 col-md-6 col-sm-12 col-12'>
+          <div className='p-2 m-1'>
+            <p>شماره پشتیبانی:09127622665</p>
+            <p>پشتیبانی ساعات اداری از 8:30 الی 17 عصر</p>
           </div>
-          <div className='col-lg-3 col-md-6 col-sm-12 col-12'>
-            <div className='p-2 m-1'>
-              <p>شماره پشتیبانی:09127622665</p>
-              <p>پشتیبانی ساعات اداری از 8:30 الی 17 عصر</p>
-            </div>
-            <div className='m-1 p-2'>
-             
-            </div>
-            <div className='m-1 p-2'>
+          <div className='m-1 p-2'>
+            <form action="">
               <h5>عضویت در خبر نامه</h5>
-              <form action="">
-              <input type="text" placeholder='وارد کردن ایمیل' className='m-2'/>
-              <input type="submit" value={'ثبت'}/>
-              </form>
-              
-              
-            </div>
-            <div className='m-1 p-2'>
-              <h5>مجوزها</h5>
-            </div>
+              <input type="text" placeholder='وارد کردن ایمیل' className='m-2' />
+              <input type="submit" value={'ثبت'} />
+            </form>
+          </div>
+          <div className='m-1 p-2'>
+            <h5>مجوزها</h5>
           </div>
         </div>
       </div>
+    </div>
   )
 }
 
