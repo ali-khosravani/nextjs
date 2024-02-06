5e540980b1c3db53f55e5e0ed60c32d18dcb568f
ali-khosravani <ali.p3soft@gmail.com>
2024-02-06 23:19:35 +0330

    No change

--- app/components/ui/Navbar/index.tsx
+++ app/components/ui/Navbar/index.tsx
@@ -4,10 +4,9 @@ import './Styled.css'
 import '@/styles/bootstrapBreakpoints.css';
 
 export const NavbarHeader = () => {
-  
-  const [navbarOpen, setNavbarOpen] = useState(false)
-  
+  const [collapsOpen, setCollapseOpen] = useState(false)
   return (
+
     <nav className="navbar navbar-expand-md bg-light justify-content-between border-bottom border-3">
       <div className="navbar-brand ms-3">
         <a href="/" className="text-black">شکوفالند</a>
@@ -15,17 +14,17 @@ export const NavbarHeader = () => {
       <button className="navbar-toggler toggle" type="button" data-bs-toggle="collapse"
         data-bs-target="#navbarcollapse" aria-controls="navbarcollapse"
         aria-expanded="true" aria-label="Toggle navigation"
-        onClick={() => setNavbarOpen(prev => !prev)}
+        onClick={() => setCollapseOpen(prev => !prev)}
       >
         {/* {!navbarOpen ? 'Open' : 'Close'} */}
         <span className="navbar-toggler-icon"></span>
-      </button>      
-      <div className={`justify-content-around ${navbarOpen ? 'menu-nav show-menu ' : 'navbar-collapse collapse'}`} id="navbarcollapse">
+      </button>
+      <div className={`${collapsOpen ? 'menu-nav show-menu ' : 'navbar-collapse collapse justify-content-around'}`} id="navbarcollapse">
         <ul className="navbar-nav home-icon">
           <li className="nav-item p-3 "><a href="/"><i className="bi bi-house-fill"></i></a></li>
-          <li className="nav-item p-3"><a href="/attendance">کنترل تردد</a></li>
-          <li className="nav-item p-3"><a href="/hardware">سخت افزار</a></li>
-          <li className="nav-item p-3"><a href="/software">نرم افزار</a></li>
+          <li className="nav-item p-3">کنترل تردد</li>
+          <li className="nav-item p-3 dropdown"><a href="/hardware">سخت افزار</a></li>
+          <li className="nav-item p-3 dropdown"><a href="/software">نرم افزار</a></li>
           <li className="nav-item p-3"><a href="/blog">بلاگ</a></li>
           <li className="nav-item p-3"><a href="/Contact">ارتباط با ما</a></li>
           <li className="nav-item p-3"><a href="/About">درباره ما</a></li>
