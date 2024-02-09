5273497109fe0367e28427c4d2c86a756d4f67e3
ali-khosravani <ali.p3soft@gmail.com>
2024-02-10 01:16:36 +0330

    fix bug in slider

--- app/components/slider/Slider.tsx
+++ app/components/slider/Slider.tsx
@@ -1,31 +1,29 @@
 "use client"
 import React from 'react'
-
 import { useState } from "react";
-import { items } from "@/public/images/Slider/Slider.json";
+import  Slide  from './json';
 import { Carousel } from "react-bootstrap";
 
-
 export const Slider = () => {
-    const { bootstrap } = items;
+
     const [index, setIndex] = useState(0);
     const handleSelect = (selectedIndex: any) => {
         setIndex(selectedIndex);
     };
     return (
-       <div className='d-none d-lg-block'>
-         <Carousel activeIndex={index} onSelect={handleSelect}>
-            {bootstrap.map((item) => (
-                <Carousel.Item key={item.id} className={'h-100 w-100'} interval={10000}>
-                    <img src={item.imageUrl} alt="New Slide" />
-                    <Carousel.Caption className={'text-center'}>
-                        <h3>{item.title}</h3>
-                        <p>{item.body}</p>
-                        <button className="btn btn-info">بیشتر</button>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            ))}
-        </Carousel>
-       </div>
+        <div className='d-none d-lg-block'>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                {Slide.items.map((item) => (
+                    <Carousel.Item key={item.id} className={'h-100 w-100'} interval={10000}>
+                        <img src={item.imageUrl} alt="New Slide" />
+                        <Carousel.Caption className={'text-center'}>
+                            <h3>{item.title}</h3>
+                            <p>{item.body}</p>
+                            <button className="btn btn-info">بیشتر</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+        </div>
     );
 }
---
+++ app/components/slider/json.tsx
@@ -0,0 +1,26 @@
+export default 
+    {
+        "items": [
+            {
+                "id": 1,
+                "title": "Access Control",
+                "body": "Bootstrap Carousel Example",
+                "imageUrl": "./images/Slider/1.png",
+                "docs": ""
+            },
+            {
+                "id": 2,
+                "title": "ZKTeco",
+                "body": "Bootstrap Carousel Example",
+                "imageUrl": "./images/Slider/2.png",
+                "docs": ""
+            },
+            {
+                "id": 3,
+                "title": "Attendance",
+                "body": "Bootstrap Carousel Example",
+                "imageUrl": "./images/Slider/3.png",
+                "docs": ""
+            }
+        ]
+    }
