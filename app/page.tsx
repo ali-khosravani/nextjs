3583bf047a865d4a6c77b9d9ccb2cc7db0444efd
ali-khosravani <ali.p3soft@gmail.com>
2024-02-11 15:28:55 +0330

    Improved performance

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,19 @@ import OurTeam from "@/com/team/OurTeam";
 import OurCustomers from "./components/customers/OurCustomers";
 import { Slider } from "./components/slider/Slider";
 
-export default function Home() {
+export default function Home() {  
   return (
     <>
       <Header />
       <Slider />
       <main className={styles.main}>
-
         <div className={styles.description}>
         </div>
-
         <div className={styles.center}>
           <h1>شکوفالند</h1>
         </div>
-
         <div className={styles.grid}>
         </div>
-
       </main>
       <OurCustomers />
       <OurTeam />
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import Image from 'next/image'
-
 import ComingSoon from '@/public/images/coming-soon.png'
 import 'bootstrap/dist/css/bootstrap.rtl.css'
 
-const Blog = () => {
+
+export default function Blog({posts}:any) {
+  const showPost = posts.map((post:any)=><p className='text-center' key={post.id}><a href="/">{post.title}</a></p>)
   return (
     <div className='vh-100 d-flex justify-content-center align-items-center'>
       <div className=''>
-        <Image src={ComingSoon} alt='' />
-        <p className='text-center'><a href="/">Back to home</a></p>
+        <Image src={ComingSoon} alt='' priority />
+        {showPost}
       </div>
     </div>
   )
 }
-export default Blog
+
+export async function getStaticProps() {
+  const posts=[
+    {
+      id:1,
+      title:'Go back to home'
+    }
+  ]
+  return{
+    props:{
+      posts
+    }
+  }
+}
