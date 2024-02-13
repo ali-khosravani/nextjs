e4a9577ecca5152192a56f2cc907f0e47174dc4d
ali-khosravani <ali.p3soft@gmail.com>
2024-02-13 23:32:18 +0330

    Update Hardware page

---
+++ pages/hardware/index.tsx
@@ -0,0 +1,9 @@
+import CommingSoonImage from '@/com/ui/CommingSoon/CommingSoon'
+import React from 'react'
+export default function Hardware() {
+  return (
+    <>
+      <CommingSoonImage />
+    </>
+  )
+}
