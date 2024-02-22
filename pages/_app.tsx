e5b2c9be111f85b3ac60c3063fda31cd25c2d680
ali-khosravani <ali.p3soft@gmail.com>
2024-02-22 23:35:38 +0330

    Add _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,10 @@ import { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.rtl.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-
     return (
         <>
             <Component {...pageProps} />
         </>
     )
-
 }
 export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Main, NextScript, Html } from "next/document";
+import React, { Component } from 'react'
+
+export default class MyDocument extends Component {
+    static async getInitialProps(ctx: any) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+    render() {
+        return (
+            <Html lang="fa" dir="rtl">
+                <Head>
+
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
