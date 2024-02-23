d21fbbfc159a0c5d99cd5af4480c78017cff4895
ali-khosravani <ali.p3soft@gmail.com>
2024-02-23 12:25:14 +0330

    Update login Form UI

--- pages/users/Login.tsx
+++ pages/users/Login.tsx
@@ -15,26 +15,22 @@ export default function Login() {
         </title>
       </Head>
       <div className='d-flex justify-content-center vh-100 align-items-center'>
-        <div className='bg-light border rounded d-flex align-items-center' style={{ height: '500px', width: '400px' }}>
+        <div className='bg-light border rounded d-none d-md-flex align-items-center' style={{ height: '600px', width: '400px' }}>
           <div className='my-5'>
             <Image src={Logo} height={100} width={400} priority alt='Logo' />
             <h1>شکوفالند</h1>
           </div>
         </div>
-        <div className='bg-light border rounded' style={{ height: '500px', width: '400px' }}>
+        <div className='bg-light border rounded' style={{ height: '600px', width: '400px' }}>
           <form action="">
             <div className='w-100 h-25 text-center my-5'>
               <h1>شکوفالند</h1>
             </div>
-            <div className='w-100 my-5'>
-              <div className='container'>
-                <h3 className='text-start px-5'>نام کاربری</h3>
-                <input type="text" placeholder='نام کاربری' className='text-start px-2' />
-              </div>
-              <div className='container'>
-                <h3 className='text-start px-5'>رمز عبور</h3>
-                <input type="password" placeholder='رمز عبور' className='text-start px-2' />
-              </div>
+            <div className='w-100 justify-content-center my-5 px-5'>              
+                <div className='h4 text-start my-3'>نام کاربری</div>
+                <input type="text" placeholder='نام کاربری' className='text-start px-5 w-100 form-control' />              
+                <div className='h4 text-start my-3'>رمز عبور</div>
+                <input type="password" placeholder='رمز عبور' className='text-start px-5 w-100 form-control' />             
             </div>
             <div className="form-check text-start mx-5">
               <input className="form-check-input" type="checkbox" checked value="" id="flexCheckDefault" />
@@ -44,7 +40,12 @@ export default function Login() {
             <div className='my-5 d-flex justify-content-center mx-5'>
               <input type="submit" value='ورود' className='w-100 btn btn-primary' />
             </div>
+            <div className='d-flex justify-content-around container'>
+              <p className='p-0 m-0'><a href="/Rule">قوانین سایت</a></p>
+              <p className='p-0 m-0'><a href="/Privacy">حریم خصوصی</a></p>
+            </div>
           </form>
+
         </div>
       </div>
     </>
