3b4f4f983c45996ae222da60dbe2f3a349cfc121
ali-khosravani <ali.p3soft@gmail.com>
2024-02-25 10:00:06 +0330

    Update Product UI

--- app/components/products/AccessControl/index.tsx
+++ app/components/products/AccessControl/index.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 const AccessControl = () => {
     return (
         <>
-            <div className="container my-5">
-                <div className="row">
-                    <div className="col">
-                        <h1>
-                            کنترل تردد
-                        </h1>
+            <div className='vh-100'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h1>
+                                کنترل تردد
+                            </h1>
+                        </div>
                     </div>
                 </div>
             </div>
--- app/components/products/Order/index.tsx
+++ app/components/products/Order/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
+import './styled.css'
 
 const Order = () => {
   return (
     <>
-    <div className='my-5'>
-        <div className="container">
-            <div className="row">
-                <h4>
-                    جهت راهنمایی و دریافت مشاوره و ثبت سفارش لطفا با شماره 0912762266  تماس حاصل نمائید
-                </h4>
-            </div>
-        </div>
-    </div>
+      <div className='my-5 container-fluid'>
+        <div className='container d-flex justify-content-center rounded-3 align-items-center'  style={{ backgroundColor: '#33f', height: '100px' }}>          
+            <h3 className='m-0'>
+              جهت راهنمایی و دریافت مشاوره و ثبت سفارش لطفا با شماره 0912762266  تماس حاصل نمائید
+            </h3>
+          </div>
+        </div>      
     </>
   )
 }
