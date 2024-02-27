5ef4abac0c9d69b7e3cc15f19155c1697cd20758
ali-khosravani <ali.p3soft@gmail.com>
2024-02-27 22:47:13 +0330

    Update ui

--- app/components/products/Category/index.tsx
+++ app/components/products/Category/index.tsx
@@ -4,50 +4,50 @@ import './styled.css'
 const Category = () => {
     return (
         <>
-            <div className='container-fluid my-5'>
+            <div className='container my-5'>
                 <div className='row'>
                     <div className="col-12 col-lg-4 mb-2">
                         <div className='container'>
                             <div>
-                                <h1>نرم افزار</h1>
+                                <h3 className='text-start fw-bold'>نرم افزار</h3>
                             </div>
-                            <div>
-                                <ul>
-                                    <li>نرم افزار</li>
-                                    <li>نرم افزار</li>
-                                    <li>نرم افزار</li>
-                                    <li>نرم افزار</li>
-                                    <li>نرم افزار</li>
+                            <div className='ul-box py-3'>
+                                <ul className='text-start'>
+                                    <li className='li-Network'><i className="bi bi-check-lg"></i>نرم افزار کنترل تردد</li>
+                                    <li className='li-Network'><i className="bi bi-check-lg"></i>نرم افزار مراحعین</li>
+                                    <li className='li-Network'><i className="bi bi-check-lg"></i>نرم افزار مدیریت پارکینگ</li>
+                                    <li className='li-Network'><i className="bi bi-check-lg"></i>نرم افزار گشت و نگهبانی</li>
+                                    <li className='li-Network'><i className="bi bi-check-lg"></i>نرم افزار حضور و غیاب</li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                     <div className="col-12 col-lg-4  mb-2">
                         <div className='container'>
-                            <h1> سخت افزار</h1>
+                            <h3 className='text-start fw-bold'> سخت افزار</h3>
                         </div>
-                        <div>
-                            <ul>
-                                <li>سخت افزار</li>
-                                <li>سخت افزار</li>
-                                <li>سخت افزار</li>
-                                <li>سخت افزار</li>
-                                <li>سخت افزار</li>
+                        <div className='ul-box py-3'>
+                            <ul className='text-start'>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>انواع راهبند</li>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>انواع گیت تردد</li>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>آنتن UHF</li>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>انواع Board</li>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>کنترل دسترسی آسانسور</li>
                             </ul>
                         </div>
                     </div>
                     <div className="col-12 col-lg-4">
                         <div className='container'>
-                            <h1>شبکه</h1>
+                            <h3 className='text-start fw-bold'>شبکه</h3>
                         </div>
-                        <div>
-                            <ul>
-                                <li>شبکه</li>
-                                <li>شبکه</li>
-                                <li>شبکه</li>
-                                <li>شبکه</li>
-                                <li>شبکه</li>
-                                <li>شبکه</li>
+                        <div className='ul-box py-3'>
+                            <ul className='text-start'>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>سوئیچ</li>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>روتر</li>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>کابل شبکه</li>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>انواع کانکتور</li>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>طراحی و اجرای اتاق سرور</li>
+                                <li className='li-Network'><i className="bi bi-check-lg"></i>انواع آنتی ویروس</li>
                             </ul>
                         </div>
                     </div>
