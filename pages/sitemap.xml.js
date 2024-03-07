8b0e22ecf751b6443925d17dee55fec1729233c3
ali-khosravani <ali.p3soft@gmail.com>
2024-03-07 20:58:03 +0330

    Add robots

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,13 @@
+function SiteMap() {
+    return 
+    `<? xml version = "1.0" encoding = "UTF-8" ?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <url>
+                <loc>http://www.shekofa-land.ir/</loc>
+                <lastmod>2024-01-01</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.8</priority>
+            </url>
+        </urlset>`
+}
+export default SiteMap;
