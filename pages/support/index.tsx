93c2ee8c5ebcd9878cafd888ebe301fe9da64ff3
ali-khosravani <ali.p3soft@gmail.com>
2024-03-29 00:06:40 +0330

    Add Suppport page

--- pages/support/index.tsx
+++ pages/support/index.tsx
@@ -1,14 +1,44 @@
+import LinkBackToHome from '@/com/ui/BackToHome/Link';
 import Head from 'next/head';
-import CommingSoonPage from '@/com/ui/ComingSoon/ComingSoon';
+
 export default function Support() {
   return (
     <>
-    <Head>
-      <title>
-        پضتیبانی رسمی محصولات
-      </title>
-    </Head>
-      <CommingSoonPage />
+      <Head>
+        <title>
+          پضتیبانی رسمی محصولات
+        </title>
+      </Head>
+      <div className='vh-100'>
+        <div className='w-100 h-25 d-flex justify-content-center align-items-center bg-dark'>
+          <div className=''>
+            <h1 className='text-white m-0 p-0'>شکوفالند</h1>
+            <p className='m-2 p-0 text-white'>راهکاری جامع برای کنترل دسترسی در سازمان شما</p>
+          </div>
+        </div>
+        <div className='container my-5'>
+          <div className="row">
+            <div className="col-12 col-lg-4">
+              <h1>
+                پشتیبانی سخت افزار
+              </h1>
+            </div>
+            <div className="col-12 col-lg-4">
+              <h1>
+                پشتیبانی نرم افزار
+              </h1>
+            </div>
+            <div className="col-12 col-lg-4">
+              <h1>
+                خدمات گارانتی
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="my-2">
+          <LinkBackToHome />
+        </div>
+      </div>
     </>
   )
 }
